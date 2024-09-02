c9365334a3dfdc6568aa6abce6a33c2473b4da19
Sesethu Mhlana <smhlana98@gmail.com>
2024-09-02 23:23:34 +0200

    Display list of books in expansion panel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { books } from './data';
 import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatExpansionModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'Book Inventory';
-  listOfBooks = books;
+  listOfBooks: Book[] = books;
+  readonly panelOpenState = signal(false);
 
   ngOnInit(): void {
     console.log(this.listOfBooks);
