ddea615169648dda9431be745389e7d0b38995f8
Sesethu Mhlana <smhlana98@gmail.com>
2024-09-03 22:45:35 +0200

    Add form to add new book.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { books } from './data';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Book } from './book';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatExpansionModule],
+  imports: [RouterOutlet, CommonModule, MatExpansionModule, ReactiveFormsModule, MatButtonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -17,8 +19,19 @@ export class AppComponent implements OnInit {
   title = 'Book Inventory';
   listOfBooks: Book[] = books;
   readonly panelOpenState = signal(false);
+  bookForm = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    genre: new FormControl(''),
+    publicationYear: new FormControl(2024),
+    isbn: new FormControl(''),
+  });
 
   ngOnInit(): void {
     console.log(this.listOfBooks);
+  }
+
+  onSubmit() {
+    console.log(this.bookForm.controls);
   }
 }
